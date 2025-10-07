54ed58bfb894659ddb26face7706960ae2414525
Parah <122792967+ParahDime@users.noreply.github.com>
2025-10-07 14:52:48 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import math
+import random
+import numpy
+#importing any classes
+import order
+
+
+#function for normalising string names
+def normaliseString():
+    #check for whitespace
+    #import all to lowercase
+    #remove any duplicates
+    pass
+#function for creating orders
+def createOrder():
+    pass
+
+
+
+#initialise variables
+
+items = ['apple', 'banana', 'orange', 'grape', 'pepper', 'plum', 'strawberry', 'pineapple', 'apricot']
+orders = []
+
+
+
---
+++ order.py
@@ -0,0 +1,6 @@
+class Orders:
+    def __init__(self, id, product_name, quantity, timestamp):
+        self.id = id
+        self.product_name = product_name
+        self.quantity = quantity
+        self.timestamp = timestamp
